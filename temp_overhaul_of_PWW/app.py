2e49489c235eb6c4f8cfffc590d2eade1228cc8a
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-10 22:42:42 +0200

    fixed paths

--- temp_overhaul_of_PWW/app.py
+++ temp_overhaul_of_PWW/app.py
@@ -5,6 +5,7 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route("/")
 def index():
+    print(url_for("templates", filename="spreadsheet_view.html"))
     return render_template("landing_page.html")
 
 

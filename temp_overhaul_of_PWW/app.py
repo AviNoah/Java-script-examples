12a30bf276b1b81841a1424af5de34329a18f4aa
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-11 00:55:52 +0200

    Fixed paths

--- temp_overhaul_of_PWW/app.py
+++ temp_overhaul_of_PWW/app.py
@@ -5,8 +5,17 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route("/")
 def index():
-    url = url_for("templates", filename="landing_page.html")
-    return render_template(url)
+    return render_template("landing_page.html")
+
+
+@app.route("/select_file")
+def file_select():
+    return render_template("file_select_grid.html")
+
+
+@app.route("/spreadsheet_view")
+def show_spreadsheet():
+    return render_template("spreadsheet_view.html")
 
 
 if __name__ == "__main__":

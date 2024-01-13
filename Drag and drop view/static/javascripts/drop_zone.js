662c53603f1e3b8b51db97bb7f444d72825fb52b
Avi Noah <AviNoah2019@gmail.com>
2024-01-14 00:32:06 +0200

    Drop zone now appears if file is anywhere on the page, and becomes invisible when a file isnt dragged to it

--- Drag and drop view/static/javascripts/drop_zone.js
+++ Drag and drop view/static/javascripts/drop_zone.js
@@ -1,21 +1,31 @@
-// Wait for DOC content to load
 document.addEventListener('DOMContentLoaded', function () {
-    const dropzone = document.querySelector('.dropzone');
-
-    dropzone.addEventListener('dragover', (e) => {
+    // Add dragover event listener to the entire document
+    document.addEventListener('dragover', function (e) {
         e.preventDefault();
-        dropzone.classList.add('dragover');
+        handleDragOver();
     });
 
-    dropzone.addEventListener('dragleave', () => {
-        dropzone.classList.remove('dragover');
+    // Add dragleave event listener to the entire document
+    document.addEventListener('dragleave', function () {
+        handleDragLeave();
     });
 
-    dropzone.addEventListener('drop', (e) => {
+    // Add drop event listener to the entire document
+    document.addEventListener('drop', function (e) {
         e.preventDefault();
-        dropzone.classList.remove('dragover');
+        handleDragLeave();
         handle_dropped_files(e);
     });
+
+    function handleDragOver() {
+        const dropzone = document.querySelector('.dropzone');
+        dropzone.classList.add('dragover');
+    }
+
+    function handleDragLeave() {
+        const dropzone = document.querySelector('.dropzone');
+        dropzone.classList.remove('dragover');
+    }
 
     function handle_dropped_files(event) {
         const files = event.dataTransfer.files;

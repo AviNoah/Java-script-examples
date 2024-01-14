99231f01468d2f84f28709ae33333f97f70cd449
Avi Noah <AviNoah2019@gmail.com>
2024-01-14 05:01:21 +0200

    moved addfiles to drop_zone js

--- File folder view/static/javascripts/drop_zone.js
+++ File folder view/static/javascripts/drop_zone.js
@@ -1,4 +1,3 @@
-import { addFiles } from './file_container_handler';
 class DragAndDropZone extends HTMLElement {
     constructor() {
         super();
@@ -40,6 +39,26 @@ class DragAndDropZone extends HTMLElement {
         console.log('Dropped Files:', files);
         addFiles(event, files)
     }
+}
+
+function addFiles(event, files) {
+    // Get parent from event, in parent search for the div.files-folder
+    const parentElement = event.target.parentElement; // Change this line based on your actual HTML structure
+
+    // Get or create the files folder div
+    let filesFolderDiv = parentElement.querySelector('.files-folder');
+    if (!filesFolderDiv) {
+        filesFolderDiv = document.createElement('div');
+        filesFolderDiv.classList.add('files-folder');
+        parentElement.appendChild(filesFolderDiv);
+    }
+
+    // Populate the files folder div with files
+    files.forEach(file => {
+        const fileElement = document.createElement('div');
+        fileElement.textContent = file.name;
+        filesFolderDiv.appendChild(fileElement);
+    });
 }
 
 // Define the custom element

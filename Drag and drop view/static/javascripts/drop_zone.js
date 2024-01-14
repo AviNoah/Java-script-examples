601641d2cc150f102edc2bce5dc193dfbf3c461e
Avi Noah <AviNoah2019@gmail.com>
2024-01-14 03:35:52 +0200

    fixed styling for drop_zone

--- Drag and drop view/static/javascripts/drop_zone.js
+++ Drag and drop view/static/javascripts/drop_zone.js
@@ -1,30 +1,14 @@
 class DragAndDropZone extends HTMLElement {
     constructor() {
         super();
-
-        // Create a shadow root
-        this.attachShadow({ mode: 'open' });
-
-        // Fetch the external CSS file and apply it to the shadow root
-        fetch('../static/styles/drop_zone_styles.css')
-            .then(response => response.text())
-            .then(cssText => {
-                // Create a style element and apply the CSS text
-                const styleElement = document.createElement('style');
-                styleElement.textContent = cssText;
-
-                // Append the style element and the dropzone container to the shadow root
-                this.shadowRoot.appendChild(styleElement);
-                this.shadowRoot.innerHTML += `
-                    <div id="dropzone_main_page" class="dropzone">
-                        <!-- Add any content you want within the custom element -->
-                    </div>
-                `;
-            })
-            .catch(error => console.error('Error fetching CSS:', error));
     }
 
     connectedCallback() {
+        // Set default size to 80% of the viewport
+        this.style.width = '80vw';
+        this.style.height = '80vh';
+
+
         document.addEventListener('dragenter', this.handleDragEnter.bind(this));
         document.addEventListener('dragleave', this.handleDragLeave.bind(this));
         document.addEventListener('dragover', this.handleDragOver.bind(this));

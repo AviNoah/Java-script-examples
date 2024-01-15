7bf53ed093fb1ebba5827da5fef9cd7ef98f0bee
Avi Noah <AviNoah2019@gmail.com>
2024-01-15 14:55:24 +0200

    file_data stores dicts now instead of tuples, better data structure; improved code readibility and used textContent instead of text

--- File folder view/static/javascripts/filter.js
+++ File folder view/static/javascripts/filter.js
@@ -1,71 +1,68 @@
 // A script to create filter elements and populate the filter lists of a file-view
 
 // files will contain a dictionary of {file_name: file_data}
-// file_data will contain a list of tuples: (column to filter by and then, {method: input}) 
+// file_data will contain a list of dicts with keys column, method and input
 const files = {
     'test_file.xlsx':
         [
-            ("ID", { 'regex': '\d+' }), ("Name", { 'contains': 'Avi' }), ("Surname", { 'contains': 'Noah' })
+            { column: "ID", method: 'regex', input: '\d+' },
+            { column: "Name", method: 'contains', input: 'Avi' },
+            { column: "Surname", method: 'contains', input: 'Noah' }
         ]
 }
 
-
-function populate(targetElement) {
-    const filters = requestFileData(targetElement);
-    const filter_div = document.createElement('div');
-    filter_div.classList.add('filters_container');
-
-    Array.from(filters).forEach((filter_data) => {
-        filter_div.appendChild(populateFilter(filter_data));
-    })
-
-    targetElement.appendChild(filter_div)
-}
-
-selectorOptions = [
+const selectorOptions = [
     { value: 'exact', text: 'Exact match' },
     { value: 'contains', text: 'Contains' },
     { value: 'not contains', text: 'Does not contain' },
     { value: 'regex', text: 'RegEx' }
 ];
 
+function populate(targetElement) {
+    const filters = requestFileData(targetElement);
+    const filter_div = document.createElement('div');
+    filter_div.classList.add('filters_container');
+
+    filters.forEach((filter_data) => {
+        filter_div.appendChild(populateFilter(filter_data));
+    })
+
+    targetElement.appendChild(filter_div);
+}
+
 function populateFilter(filter_data) {
-    // (column name: {method: input})
-    const column = filter_data[0];
-    const method = Object.keys(filter_data[1]);
-    const filter_input = filter_data[1][method];
+    const { column, method, input } = filter_data;
 
     const filter = document.createElement('div');
-    filter.classList.add('filter wrapper');
+    filter.classList.add('filter', 'wrapper');
 
     const selector = document.createElement('select');
     selector.classList.add('filter-selector');
 
     // Iterate through the data and add options to the select element
-    selectorOptions.forEach(item => {
+    selectorOptions.forEach((item) => {
         const option = document.createElement('option');
-        option.classList.add("filter-option");
+        option.classList.add('filter-option');
         option.value = item.value;
         option.text = item.text;
 
-        if (option.value === column)
+        if (option.value === method)
             option.selected = true;
 
-        selectElement.appendChild(option);
+        selector.appendChild(option);
     });
 
     const inpField = document.createElement('input');
-    inpField.type = "text";
-    inpField.value = filter_input;
+    inpField.type = 'text';
+    inpField.value = input;
 
     const submitButton = document.createElement('button');
     submitButton.classList.add('filter-submit');
-    submitButton.text = "Submit";
-    submitButton.addEventListener("click", () => { submit(selector, inpField) });
+    submitButton.textContent = 'Submit';
+    submitButton.addEventListener('click', () => { submit(column, selector, inpField) });
 
     const deleteButton = document.createElement('button');
-    deleteButton.classList.add('filter-delete');
-    deleteButton.classList.add('dangerous'); // elements marked dangerous will require confirm
+    deleteButton.classList.add('filter-delete', 'dangerous'); // elements marked dangerous will require confirm
 
     filter.appendChild(selector);
     filter.appendChild(inpField);
@@ -75,11 +72,12 @@ function populateFilter(filter_data) {
     return filter;
 }
 
-function submit(selector, inpField) {
-    const selection = selector.value;
+function submit(column, selector, inpField) {
+    const method = selector.value;
     const input = inpField.value;
 
     // Send to back end to process, implement later
+    console.log(`Column: ${column}, Method: ${method}, Input: ${input}`);
 }
 
 function requestFileData(targetElement) {

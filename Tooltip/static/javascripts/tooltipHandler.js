4cbc14976227227c3e0654653954e762a297ae80
Avi Noah <AviNoah2019@gmail.com>
2024-02-19 12:43:01 +0200

    Added word-wrap; now position tooltip next to hovered text

--- Tooltip/static/javascripts/tooltipHandler.js
+++ Tooltip/static/javascripts/tooltipHandler.js
@@ -2,10 +2,17 @@ const tooltipTriggerList = document.querySelectorAll('.tooltip');
 const tooltip = document.getElementById('tooltip');
 
 tooltipTriggerList.forEach((tooltipTriggerEl) => {
-    tooltipTriggerEl.addEventListener('mouseover', function () {
+    tooltipTriggerEl.addEventListener('mouseover', function (event) {
         const tooltipText = this.getAttribute('data-tooltip');  // Fetch tooltip text
+        const tooltipX = event.clientX + 10; // Adjust 10px to the right
+        const tooltipY = event.clientY + 10; // Adjust 10px downward
+
         tooltip.innerHTML = tooltipText;
+        tooltip.style.left = tooltipX + 'px';
+        tooltip.style.top = tooltipY + 'px';
         tooltip.classList.remove('hidden');
+
+
     });
     tooltipTriggerEl.addEventListener('mouseout', function () {
         tooltip.classList.add('hidden');
